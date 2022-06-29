cb1c27adc76f719809d7af4a5de3d5521de6081e
Bruno Vinícius Souza de Sá <brunovinciussouza.sa@gmail.com>
2022-06-29 14:17:26 -0300

    Spoonacular search API working properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import Main from './pages/main';
+
 export default function App() {
-  return <div className='App'>opa</div>;
+  return (
+    <div className='App'>
+      <Main />
+    </div>
+  );
 }
--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export const apiKey = 'fe1c16c2f10d441bb87b9e1ce6ebb677';
+const API_URL = 'https://api.spoonacular.com/recipes';
 
-export const api = axios.create({
-  baseURL: 'https://api.spoonacular.com/recipes',
+export const API_KEY = 'fe1c16c2f10d441bb87b9e1ce6ebb677';
+
+export const API = axios.create({
+  baseURL: API_URL,
 });
