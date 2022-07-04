462151e408c3910a3274ed59e0f8e6c2eec229ab
Bruno Vinícius Souza de Sá <brunovinciussouza.sa@gmail.com>
2022-07-04 18:51:12 -0300

    main page and header adjustments

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,11 +1,26 @@
 import { Header } from '../../components/Header';
 import bg from '../../assets/svg/cooking.svg';
 
+import './styles.scss';
+import { Link } from 'react-router-dom';
+
 export default function Main() {
   return (
-    <div>
+    <>
       <Header />
-      <img alt='img' src={bg}></img>
-    </div>
+      <div className='container'>
+        <section className='main-content'>
+          <h1>Enjoy food from all over the world</h1>
+          <p>
+            Healthy recipes shake up your comfort food favorites by adding more
+            veggies and swapping out cooking method
+          </p>
+          <img alt='img' src={bg}></img>
+          <Link className='primary-btn' to={'/search'}>
+            Get recipes
+          </Link>
+        </section>
+      </div>
+    </>
   );
 }
