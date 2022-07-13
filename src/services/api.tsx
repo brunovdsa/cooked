be22dedde1c58541a41d4bb6a819d3c33150f73b
Bruno Vinícius Souza de Sá <brunovinciussouza.sa@gmail.com>
2022-07-13 13:44:51 -0300

    img to_readme

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const API_URL = 'https://api.spoonacular.com/';
 
-export const API_KEY = 'fe1c16c2f10d441bb87b9e1ce6ebb677';
+export const API_KEY = 'd6975baa123a40de893534a79f73c14a';
 
 export const API = axios.create({
   baseURL: API_URL,
