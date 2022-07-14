d239373362f4f5735c4d05b4a80da1d8dc8da2e8
Bruno Vinícius Souza de Sá <brunovinciussouza.sa@gmail.com>
2022-07-14 00:03:11 -0300

    card-link improved

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,17 +6,19 @@ import './styles.scss';
 export default function Card(props: ICard) {
   return (
     <div className='card' key={props.id}>
-      {props.image.length ? (
-        <img width={'100%'} src={props.image} alt='' />
-      ) : (
-        <div>No Image</div>
-      )}
-      <div className='card-info'>
-        <div className='card-title'>{props.title}</div>
-        <Link className='card-link' to={`/recipe/${props.id}`}>
-          Ingredients
-        </Link>
-      </div>
+      <Link className='card-link' to={`/recipe/${props.id}`}>
+        {props.image.length ? (
+          <img width={'100%'} src={props.image} alt='' />
+        ) : (
+          <div>No Image</div>
+        )}
+        <div className='card-info'>
+          <div className='card-title'>{props.title}</div>
+          <Link className='card-link' to={`/recipe/${props.id}`}>
+            Ingredients
+          </Link>
+        </div>
+      </Link>
     </div>
   );
 }
